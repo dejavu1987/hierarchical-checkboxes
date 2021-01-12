fa3de542cfe624573808470661197bfd1ac99ee9
Anil Maharjan <anil@anilmaharjan.com.np>
2021-01-12 20:40:59 +0100

    Add data along with the change event

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Hierarchical checkboxes
 
+jQuery for hierarchical checkboxes
 
-jQuery for hierarchical checkboxes 
+## DEMO
 
+[CodeSandbox](https://vceg4.csb.app/demo.html)
 
 ### USAGE:
 
@@ -12,11 +14,11 @@ Include 'hierarchical-checkboxes.css'
 All the HTML elements with class hierarchy-checkboxes will be parsed into the hierarchical checkboxes.
 You will need to add an identifier in rel attribute to distinguish between different sets of checkboxes.
 
-
 Template Construction:
 Use ROOT template and then nest as many NODE templates to create your hierarchy
 
 #### Root:
+
 ```HTML
 <div class="hierarchy-checkboxes" rel="test">
   <input class="hierarchy-root-checkbox" type="checkbox">
@@ -26,37 +28,39 @@ Use ROOT template and then nest as many NODE templates to create your hierarchy
   </div>
 </div>
 ```
+
 #### Node:
+
 ```HTML
 <div class="hierarchy-node [leaf]">
   <input class="hierarchy-checkbox" type="checkbox">
   <label class="hierarchy-label">[Title]</label>
   {{ NODE TEMPLATE HERE }}
-</div> 
-```
-
-### Basic Example Template
-```html
-<div class="hierarchy-checkboxes" rel="test">
-  <input class="hierarchy-root-checkbox" type="checkbox">
-  <label class="hierarchy-root-label">Root</label>
-  <div class="hierarchy-root-child hierarchy-node">
-   <div class="hierarchy-node leaf">
-      <input class="hierarchy-checkbox" type="checkbox">
-      <label class="hierarchy-label">Markets</label>
-      <div class="hierarchy-node leaf">
-	<input class="hierarchy-checkbox" type="checkbox">
-	<label class="hierarchy-label">Markets</label>
-      </div> 
-    </div> 
-    <div class="hierarchy-node leaf">
-      <input class="hierarchy-checkbox" type="checkbox">
-      <label class="hierarchy-label">Markets</label>
-    </div> 
-  </div>
 </div>
 ```
 
+### Basic Example Template
+
+```html
+<div class="hierarchy-checkboxes" rel="test">
+  <input class="hierarchy-root-checkbox" type="checkbox" />
+  <label class="hierarchy-root-label">Root</label>
+  <div class="hierarchy-root-child hierarchy-node">
+    <div class="hierarchy-node leaf">
+      <input class="hierarchy-checkbox" type="checkbox" />
+      <label class="hierarchy-label">Markets</label>
+      <div class="hierarchy-node leaf">
+        <input class="hierarchy-checkbox" type="checkbox" />
+        <label class="hierarchy-label">Markets</label>
+      </div>
+    </div>
+    <div class="hierarchy-node leaf">
+      <input class="hierarchy-checkbox" type="checkbox" />
+      <label class="hierarchy-label">Markets</label>
+    </div>
+  </div>
+</div>
+```
 
 ### API:
 
@@ -64,11 +68,15 @@ Use ROOT template and then nest as many NODE templates to create your hierarchy
 
 ##### 1. checkboxesUpdate:
 
-  Triggers whenever the check/uncheck tasks complete within the hierarchical checkboxes. This can be useful especially when you need to do some task only after all the states of the checkboxes are updated.
+Triggers whenever the check/uncheck tasks complete within the hierarchical checkboxes. This can be useful especially when you need to do some task only after all the states of the checkboxes are updated.
 
 Example:
+
 ```javascript
-jQuery('.hierarchy-checkboxes[rel=IDENTIFIER]').on('checkboxesUpdate',function(){
-  console.log("Changed!");
-});
+jQuery(".hierarchy-checkboxes[rel=IDENTIFIER]").on(
+  "checkboxesUpdate",
+  function () {
+    console.log("Changed!");
+  }
+);
 ```
--- hierarchical-checkboxes.js
+++ hierarchical-checkboxes.js
@@ -65,97 +65,120 @@ jQuery('.hierarchy-checkboxes[rel=IDENTIFIER]').on('checkboxesUpdate',function()
 
 **/
 
+jQuery(document).ready(function () {
+  jQuery(".hierarchy-checkboxes .hierarchy-root-child div div").hide();
+  jQuery(".hierarchy-checkboxes .hierarchy-root-child")
+    .attr("rel", function () {
+      const $this = jQuery(this);
+      $this.attr("rel", $this.closest(".hierarchy-checkboxes").attr("rel"));
+    })
+    .appendTo("body")
+    .hide();
+  jQuery(
+    ".hierarchy-checkboxes, .hierarchy-root-child .hierarchy-node"
+  ).prepend('<div class="expand-collapse-button"></div>');
+  // Root label toggles root-child / Popup layer as a whole
+  jQuery(".hierarchy-root-label").click(function () {
+    const $this = jQuery(this);
+    const $thisNode = $this.parent();
+    const rel = $thisNode.attr("rel");
+    const $rootChild = jQuery(".hierarchy-root-child[rel=" + rel + "]");
+    if (!$thisNode.hasClass("child-expanded")) {
+      $thisNode.addClass("child-expanded");
+      const thisPos = $thisNode.offset();
 
+      $rootChild
+        .css({ left: thisPos.left, top: thisPos.top + $thisNode.height() - 1 })
+        .slideDown("fast");
+    } else {
+      $rootChild.slideUp("fast", function () {
+        $thisNode.removeClass("child-expanded");
+      });
+    }
+  });
 
-jQuery(document).ready(function(){
-    jQuery('.hierarchy-checkboxes .hierarchy-root-child div div').hide();
-    jQuery('.hierarchy-checkboxes .hierarchy-root-child').attr('rel',function(){
-      $this = jQuery(this);
-      $this.attr('rel',$this.closest('.hierarchy-checkboxes').attr('rel'));
-    }).appendTo('body').hide();
-    jQuery('.hierarchy-checkboxes, .hierarchy-root-child .hierarchy-node').prepend('<div class="expand-collapse-button"></div>');
-    // Root label toggles root-child / Popup layer as a whole
-    jQuery('.hierarchy-root-label').click(function(){
-      $this = jQuery(this);
-      $thisNode = $this.parent();
-      rel = $thisNode.attr('rel');
-      $rootChild = jQuery('.hierarchy-root-child[rel='+rel+']');
-      if(!$thisNode.hasClass('child-expanded')){
-        $thisNode.addClass('child-expanded');
-        thisPos = $thisNode.offset();
-        
-        $rootChild.css({left: thisPos.left, top: thisPos.top + $thisNode.height()-1})
-        .slideDown('fast');
-      }else{
-        $rootChild.slideUp('fast',function(){$thisNode.removeClass('child-expanded');});
-      }
-    });
-    
-    jQuery('.expand-collapse-button').click(function(){
-      jQuery(this).siblings('.hierarchy-label, .hierarchy-root-label').click();
-    });
-    jQuery('.hierarchy-root-checkbox').change(function(){
-      $this = jQuery(this);
-      //$thisNode is parent to current checkbox so it would represent current level node
-      $thisNode = $this.parent();
-      
-      rel = $thisNode.attr('rel'); // Identifier (rel attribute of current hierarchy root)
-      $rootChild = jQuery('.hierarchy-root-child[rel='+rel+']'); // The node that contains all the elements of hierarchy;
-      $rootChild.find('input.hierarchy-checkbox').prop('checked', $this.prop('checked'));
-      $thisNode.trigger('checkboxesUpdate');
-    });
+  jQuery(".expand-collapse-button").click(function () {
+    jQuery(this).siblings(".hierarchy-label, .hierarchy-root-label").click();
+  });
+  jQuery(".hierarchy-root-checkbox").change(function () {
+    const $this = jQuery(this);
+    //$thisNode is parent to current checkbox so it would represent current level node
+    const $thisNode = $this.parent();
 
-    // Each node's label toggles the node's child / label's sibling
-    jQuery('.hierarchy-node .hierarchy-label').click(function(){
-      $this = jQuery(this);
-      $thisNode = $this.parent();
-      if(!$thisNode.hasClass('child-expanded')){
-        $thisNode.addClass('child-expanded');
-        $this.siblings('.hierarchy-node').slideDown('fast');
-      }else{
-        $this.siblings('.hierarchy-node').slideUp('fast',function(){$thisNode.removeClass('child-expanded');});
-        
-      }
-    });
+    const rel = $thisNode.attr("rel"); // Identifier (rel attribute of current hierarchy root)
+    const $rootChild = jQuery(".hierarchy-root-child[rel=" + rel + "]"); // The node that contains all the elements of hierarchy;
+    $rootChild
+      .find("input.hierarchy-checkbox")
+      .prop("checked", $this.prop("checked"));
+    $thisNode.trigger("checkboxesUpdate");
+  });
 
-    // Each node's checkbox toggles the node's child / checkbox's sibling
-    jQuery('.hierarchy-node .hierarchy-checkbox').change(function(){
-      $this = jQuery(this);
-      //$thisNode is parent to current checkbox so it would represent current level node
-      $thisNode = $this.parent();
-      $parentNode = $thisNode.parent('.hierarchy-node');
-      $parentNodeCheckbox = $parentNode.children('input.hierarchy-checkbox');
-      
-      $rootChild = $this.parents('.hierarchy-root-child'); // The node that contains all the elements of hierarchy;
-      rel = $rootChild.attr('rel'); // Identifier (rel attribute of current hierarchy root)
-      $root = jQuery('.hierarchy-checkboxes[rel='+rel+']');
-      $rootCheckbox = jQuery('.hierarchy-checkboxes[rel='+rel+'] .hierarchy-root-checkbox');
+  // Each node's label toggles the node's child / label's sibling
+  jQuery(".hierarchy-node .hierarchy-label").click(function () {
+    const $this = jQuery(this);
+    const $thisNode = $this.parent();
+    if (!$thisNode.hasClass("child-expanded")) {
+      $thisNode.addClass("child-expanded");
+      $this.siblings(".hierarchy-node").slideDown("fast");
+    } else {
+      $this.siblings(".hierarchy-node").slideUp("fast", function () {
+        $thisNode.removeClass("child-expanded");
+      });
+    }
+  });
 
+  // Each node's checkbox toggles the node's child / checkbox's sibling
+  jQuery(".hierarchy-node .hierarchy-checkbox").change(function () {
+    const $this = jQuery(this);
+    //$thisNode is parent to current checkbox so it would represent current level node
+    const $thisNode = $this.parent();
+    const $parentNode = $thisNode.parent(".hierarchy-node");
+    const $parentNodeCheckbox = $parentNode.children(
+      "input.hierarchy-checkbox"
+    );
 
-      // take care of children | Easy one
-      $this.siblings('.hierarchy-node').find('input.hierarchy-checkbox').prop('checked', $this.prop('checked'));
+    const $rootChild = $this.parents(".hierarchy-root-child"); // The node that contains all the elements of hierarchy;
+    const rel = $rootChild.attr("rel"); // Identifier (rel attribute of current hierarchy root)
+    const $root = jQuery(".hierarchy-checkboxes[rel=" + rel + "]");
+    const $rootCheckbox = jQuery(
+      ".hierarchy-checkboxes[rel=" + rel + "] .hierarchy-root-checkbox"
+    );
 
+    // take care of children | Easy one
+    $this
+      .siblings(".hierarchy-node")
+      .find("input.hierarchy-checkbox")
+      .prop("checked", $this.prop("checked"));
 
-      //take care of parents | tough one
-      if(!$this.prop('checked')){
-        // If unchecked uncheck all the ancestors
-        $thisNode.parents('.hierarchy-node').children('input.hierarchy-checkbox').prop('checked', $this.prop('checked'));
-        // also uncheck the root
-        $rootCheckbox.prop('checked',false);
-      }else{
-        // If checked check for the siblings state and check the parent if all siblings are checked too
-        allCheckboxesInCurrentDepth = $parentNode.find('.hierarchy-node .hierarchy-checkbox').length;
-        allCheckedCheckboxesInCurrentDepth = $parentNode.find('.hierarchy-node .hierarchy-checkbox:checked').length;
-        // all nodes in and below siblings are checked
-        if(allCheckboxesInCurrentDepth == allCheckedCheckboxesInCurrentDepth){
-          // check the parent
-          if($parentNodeCheckbox.length){
-            $parentNodeCheckbox.prop('checked',true);
-          }else{
-            $rootCheckbox.prop('checked',true);
-          }
+    //take care of parents | tough one
+    if (!$this.prop("checked")) {
+      // If unchecked uncheck all the ancestors
+      $thisNode
+        .parents(".hierarchy-node")
+        .children("input.hierarchy-checkbox")
+        .prop("checked", $this.prop("checked"));
+      // also uncheck the root
+      $rootCheckbox.prop("checked", false);
+    } else {
+      // If checked check for the siblings state and check the parent if all siblings are checked too
+      const allCheckboxesInCurrentDepth = $parentNode.find(
+        ".hierarchy-node .hierarchy-checkbox"
+      ).length;
+      const allCheckedCheckboxesInCurrentDepth = $parentNode.find(
+        ".hierarchy-node .hierarchy-checkbox:checked"
+      ).length;
+      // all nodes in and below siblings are checked
+      if (allCheckboxesInCurrentDepth === allCheckedCheckboxesInCurrentDepth) {
+        // check the parent
+        if ($parentNodeCheckbox.length) {
+          $parentNodeCheckbox.prop("checked", true);
+        } else {
+          $rootCheckbox.prop("checked", true);
         }
       }
-      $root.trigger('checkboxesUpdate');
-    });
+    }
+    $root.trigger("checkboxesUpdate", [
+      $rootChild.find(".hierarchy-checkbox:checked")
+    ]);
   });
+});
